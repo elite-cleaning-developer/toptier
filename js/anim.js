642d59bebb5ac759f679e809419beca02ceed018
Shon Danvudi <24816163@nwtc.edu>
2023-03-03 17:07:40 -0600

    v2

---
+++ js/anim.js
@@ -0,0 +1,18 @@
+
+
+
+function scrollMe()
+{
+    $(document).ready(function(){
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+       
+        		
+		
+         
+        
+   });
+}
+
+scrollMe();
