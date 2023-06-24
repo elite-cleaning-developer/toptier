0ffa3cc4a8ecc3fc28343115247586eb60ebba86
Shon Danvudi <sdanvudi@gmail.com>
2023-06-24 10:50:42 -0500

    header fix UI

--- js/anim.js
+++ js/anim.js
@@ -1,1 +1,16 @@
 
+
+
+
+function cookies(){
+    document.getElementById('policy').style.display = 'none';
+    document.getElementById('cookies').style.display = 'block';
+    document.getElementById('cookies').style.height = '70vh';
+
+}
+    function policy(){
+    document.getElementById('policy').style.display = 'block';
+    document.getElementById('cookies').style.display = 'none';
+    document.getElementById('cookies').style.height = 'fit-content';
+
+}
