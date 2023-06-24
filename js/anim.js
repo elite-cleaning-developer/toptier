c02cd8a4171e6181398a526e62553ffaf73a38cc
Shon Danvudi <sdanvudi@gmail.com>
2023-06-24 09:41:15 -0500

    ui fix

--- js/anim.js
+++ js/anim.js
@@ -1,18 +1,1 @@
 
-
-
-const home = document.getElementById('hmLnk');
-
-
-home.addEventListener('click', () =>{
-    location.replace('http://127.0.0.1:5500/')
-})
-
-function cookies(){
-    document.getElementById('policy').style.display = 'none';
-    document.getElementById('cookies').style.display = 'block';
-}
-    function policy(){
-    document.getElementById('policy').style.display = 'block';
-    document.getElementById('cookies').style.display = 'none';
-}
